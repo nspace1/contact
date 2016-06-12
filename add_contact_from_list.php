16985ce524671ec41eaea509005b6bc7b7aa6080
Nazar <naaa.nazar@gmail.com>
2016-06-12 15:27:15 +0300

    edit

--- add_contact_from_list.php
+++ add_contact_from_list.php
@@ -56,7 +56,7 @@
 							<thead>
 								<tr><th>
 										all										
-										<input type="image" name="check_all" value="check_all" src="<?= $_SESSION['cheked_all'] == '' ? 'img\checked.png' : 'img\unchecked.png'?>">
+										<input type="image" name="check_all" value="check_all" src="<?= isset($_SESSION['cheked_all']) ? $_SESSION['cheked_all'] == '' ? 'img\checked.png' : 'img\unchecked.png' : 'img\unchecked.png' ?>">
 									</th>
 																
 									

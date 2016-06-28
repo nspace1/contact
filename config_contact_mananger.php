23d92bf742daf119fdc50e74e335328a880d2945
Nazar <naaa.nazar@gmail.com>
2016-06-29 02:17:26 +0300

    - better to use default values for sorting (and storage it near main constants)   - create file with constants (DB connection, pagination params, etc...)

---
+++ config_contact_mananger.php
@@ -0,0 +1,20 @@
+<?php 
+	//db_config
+	if (file_exists('php_script\mysql_login.php')) {
+    	require_once 'php_script\mysql_login.php';    		
+	}
+	else{
+		define(DB_HOSTNAME, 'localhost');
+		define(DB_DATABASE, 'contact_mananger');
+		define(DB_USERNAME, '');
+		define(DB_PASSWORD, '');
+	}
+
+	//pagination
+	const NUM_RECORDS_PER_PAGE = 5;
+
+	//sort
+	const DEFAULT_ORDER_FIRSTNAME = 'ASC';
+	const DEFAULT_ORDER_LASTNAME = 'ASC';
+
+
--- php_script/sql_connect.php
+++ php_script/sql_connect.php
@@ -1,12 +1,14 @@
 <?php
 function  sql_connect(){
-	 	require_once 'mysql_login.php';
-	 	$conn=sql_conn($db_hostname, $db_database, $db_username, $db_password);
+	
+		require_once 'config_contact_mananger.php';
+		
+	 	$conn=sql_conn();
 	 	return $conn;
 	 }
 
-	 function sql_conn($db_hostname, $db_database, $db_username, $db_password){
- 		 	 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
+	 function sql_conn(){
+ 		 	 $conn = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
  		 	 mysqli_set_charset( $conn, 'utf8');
 			  if (mysqli_connect_errno())
 			  {
